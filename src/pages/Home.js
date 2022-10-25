0e0f2d971b137e6f405add90d1262d7a05a296ad
SANTANU KUMAR SAHOO <58762133+santanusahoo1@users.noreply.github.com>
2022-10-25 16:59:40 +0530

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,10 +5,8 @@ const Home = () => {
     return (
         <div>
             <h2 className="heading">Welcome to the Redux toolkit store</h2>
-            <section>
-                <h3>Products</h3>
-                <Products />
-            </section>
+            <h3>Products</h3>
+            <Products />
         </div>
     );
 };
